843221f18bc809d53c3e2e8609f7df1f41921795
Dongmin Jang <dogzz9445@gmail.com>
2024-06-03 08:30:02 +0900

    modify

--- src/Shared/DDT.Core.WidgetSystems/Services/WidgetSystemService.cs
+++ src/Shared/DDT.Core.WidgetSystems/Services/WidgetSystemService.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace DDT.Core.WidgetSystems.Services;
 
@@ -13,4 +15,16 @@ public interface IWidgetSystemService
 
 public class WidgetSystemService : IWidgetSystemService
 {
+
+    public WidgetSystemService()
+    {
+    }
+
+    private IConfiguration BuildConfiguration(string path)
+    {
+        var builder = new ConfigurationBuilder()
+            .AddJsonFile(path);
+
+        return builder.Build();
+    }
 }
